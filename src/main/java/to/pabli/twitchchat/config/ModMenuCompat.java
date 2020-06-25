80bdffa485681d75321ba0a1a32bb0d6921c1e97
Pablo Pérez Rodríguez <pablo.rabanales@gmail.com>
2020-06-25 14:08:39 +0200

    Update ModMenuCompat to keep up with ModMenuApi changes

--- src/main/java/to/pabli/twitchchat/config/ModMenuCompat.java
+++ src/main/java/to/pabli/twitchchat/config/ModMenuCompat.java
@@ -1,5 +1,6 @@
 package to.pabli.twitchchat.config;
 
+import io.github.prospector.modmenu.api.ConfigScreenFactory;
 import io.github.prospector.modmenu.api.ModMenuApi;
 import java.util.function.Function;
 
@@ -14,41 +15,36 @@ import net.minecraft.text.TranslatableText;
 @Environment(EnvType.CLIENT)
 public class ModMenuCompat implements ModMenuApi {
   @Override
-  public String getModId() {
-    return "twitchchat";
-  }
-
-  @Override
-  public Function<Screen, ? extends Screen> getConfigScreenFactory() {
-    return (prevScreen) -> {
+  public ConfigScreenFactory<?> getModConfigScreenFactory() {
+    return (ConfigScreenFactory<Screen>) screen -> {
       ConfigBuilder builder = ConfigBuilder.create();
-      builder.setTitle("text.twitchchat.title");
+      builder.setTitle(new TranslatableText("text.twitchchat.title"));
       builder.setSavingRunnable(() -> ModConfig.getConfig().save());
 
 
       ConfigEntryBuilder entryBuilder = ConfigEntryBuilder.create();
 
-      ConfigCategory defaultCategory = builder.getOrCreateCategory("text.twitchchat.category.default");
+      ConfigCategory defaultCategory = builder.getOrCreateCategory(new TranslatableText("text.twitchchat.category.default"));
       defaultCategory.addEntry(entryBuilder
-          .startStrField("text.twitchchat.default.username", ModConfig.getConfig().getUsername())
-          .setSaveConsumer((s -> ModConfig.getConfig().setUsername(s)))
-          .setTooltip(new TranslatableText("text.twitchchat.default.username.tooltip").asString())
-          .build());
+              .startStrField(new TranslatableText("text.twitchchat.default.username"), ModConfig.getConfig().getUsername())
+              .setSaveConsumer((s -> ModConfig.getConfig().setUsername(s)))
+              .setTooltip(new TranslatableText("text.twitchchat.default.username.tooltip"))
+              .build());
       defaultCategory.addEntry(entryBuilder
-          .startStrField("text.twitchchat.default.oauthKey", ModConfig.getConfig().getOauthKey())
-          .setSaveConsumer((s -> ModConfig.getConfig().setOauthKey(s)))
-          .setTooltip(new TranslatableText("text.twitchchat.default.oauthKey.tooltip").asString())
-          .build());
+              .startStrField(new TranslatableText("text.twitchchat.default.oauthKey"), ModConfig.getConfig().getOauthKey())
+              .setSaveConsumer((s -> ModConfig.getConfig().setOauthKey(s)))
+              .setTooltip(new TranslatableText("text.twitchchat.default.oauthKey.tooltip"))
+              .build());
       defaultCategory.addEntry(entryBuilder
-          .startStrField("text.twitchchat.default.prefix", ModConfig.getConfig().getPrefix())
-          .setSaveConsumer((s -> ModConfig.getConfig().setPrefix(s)))
-          .setTooltip(new TranslatableText("text.twitchchat.default.prefix.tooltip").asString())
-          .build());
+              .startStrField(new TranslatableText("text.twitchchat.default.prefix"), ModConfig.getConfig().getPrefix())
+              .setSaveConsumer((s -> ModConfig.getConfig().setPrefix(s)))
+              .setTooltip(new TranslatableText("text.twitchchat.default.prefix.tooltip"))
+              .build());
       defaultCategory.addEntry(entryBuilder
-          .startStrField("text.twitchchat.default.dateFormat", ModConfig.getConfig().getDateFormat())
-          .setSaveConsumer((s -> ModConfig.getConfig().setDateFormat(s)))
-          .setTooltip(new TranslatableText("text.twitchchat.default.dateFormat.tooltip").asString())
-          .build());
+              .startStrField(new TranslatableText("text.twitchchat.default.dateFormat"), ModConfig.getConfig().getDateFormat())
+              .setSaveConsumer((s -> ModConfig.getConfig().setDateFormat(s)))
+              .setTooltip(new TranslatableText("text.twitchchat.default.dateFormat.tooltip"))
+              .build());
 
       return builder.build();
     };
