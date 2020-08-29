d8ef10472d6390b2de404c269bfe372fb1efeec1
Pablo <pablo.rabanales@gmail.com>
2020-08-29 02:07:47 +0200

    Generate glyph from RenderableGlyph

---
+++ src/main/java/to/pabli/twitchchat/emotes/EmoteRenderableGlyph.java
@@ -0,0 +1,74 @@
+package to.pabli.twitchchat.emotes;
+
+import net.minecraft.client.font.Glyph;
+import net.minecraft.client.font.RenderableGlyph;
+import net.minecraft.client.texture.NativeImage;
+import net.minecraft.util.Util;
+
+public enum EmoteRenderableGlyph implements RenderableGlyph {
+   INSTANCE;
+
+   private static final NativeImage IMAGE = Util.make(new NativeImage(NativeImage.Format.ABGR, 5, 8, false), (nativeImage) -> {
+      for(int i = 0; i < 8; ++i) {
+         for(int j = 0; j < 5; ++j) {
+            boolean bl = j == 0 || j + 1 == 5 || i == 0 || i + 1 == 8;
+            nativeImage.setPixelColor(j, i, bl ? -1 : -1);
+         }
+      }
+
+      nativeImage.untrack();
+   });
+
+   public int getWidth() {
+      return 5;
+   }
+
+   public int getHeight() {
+      return 8;
+   }
+
+   public float getAdvance() {
+      return 6.0F;
+   }
+
+   public float getOversample() {
+      return 1.0F;
+   }
+
+   public void upload(int x, int y) {
+      IMAGE.upload(0, x, y, false);
+   }
+
+   public boolean hasColor() {
+      return true;
+   }
+
+   public Glyph getGlyph() {
+      return new Glyph() {
+         @Override
+         public float getAdvance() {
+            return EmoteRenderableGlyph.this.getAdvance();
+         }
+
+         @Override
+         public float getAdvance(boolean bold) {
+            return this.getAdvance();
+         }
+
+         @Override
+         public float getBearingX() {
+            return this.getAdvance();
+         }
+
+         @Override
+         public float getBoldOffset() {
+            return this.getAdvance();
+         }
+
+         @Override
+         public float getShadowOffset() {
+            return 1F;
+         }
+      };
+   }
+}
