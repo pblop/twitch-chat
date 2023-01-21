089d1ed28307ba6435d6e66a3d670f9d3ed8a7b1
pablo <pablo@pabl.eu>
2023-01-21 01:10:09 +0100

    Fix addTwitchMessage for 1.19.3

--- src/main/java/eu/pabl/twitchchat/TwitchChatMod.java
+++ src/main/java/eu/pabl/twitchchat/TwitchChatMod.java
@@ -52,7 +52,7 @@ public class TwitchChatMod implements ModInitializer {
         String plainTextMessage = ModConfig.getConfig().getBroadcastPrefix() + username + ": " + message;
         plainTextMessage = sanitiseMessage(plainTextMessage);
         if (MinecraftClient.getInstance().player != null) {
-          MinecraftClient.getInstance().player.sendChatMessage(plainTextMessage, null);
+          MinecraftClient.getInstance().player.sendMessage(Text.literal(plainTextMessage));
         }
       } catch (NullPointerException e) {
         System.err.println("TWITCH BOT FAILED TO BROADCAST MESSAGE: " + e.getMessage());
