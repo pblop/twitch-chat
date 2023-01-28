9648e2c393588919bdbd61ecb5dd90334638f1fd
pablo <pablo@pabl.eu>
2023-01-28 02:07:51 +0100

    Remove removed TwitchEnableCommand from TwitchBaseCommand

--- src/main/java/eu/pabl/twitchchat/commands/TwitchBaseCommand.java
+++ src/main/java/eu/pabl/twitchchat/commands/TwitchBaseCommand.java
@@ -9,8 +9,6 @@ import net.minecraft.text.Text;
 public class TwitchBaseCommand implements BaseCommand {
   public void registerCommands(CommandDispatcher<FabricClientCommandSource> dispatcher) {
       dispatcher.register(ClientCommandManager.literal("twitch")
-          // The command to be executed if the command "twitch" is entered with the argument "enable"
-          .then(new TwitchEnableCommand().getArgumentBuilder())
           // The command to be executed if the command "twitch" is entered with the argument "disable"
           .then(new TwitchDisableCommand().getArgumentBuilder())
           .then(new TwitchWatchCommand().getArgumentBuilder())
