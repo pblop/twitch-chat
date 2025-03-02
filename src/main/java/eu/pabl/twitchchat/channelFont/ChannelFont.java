369998047b275411567877b93e5cfd79fe9c8567
Kesuaheli <kesuaheligame@gmail.com>
2025-03-02 14:04:17 +0100

    refactor: moved font constants to font class

--- src/main/java/eu/pabl/twitchchat/channelFont/ChannelFont.java
+++ src/main/java/eu/pabl/twitchchat/channelFont/ChannelFont.java
@@ -3,12 +3,17 @@ package eu.pabl.twitchchat.channelFont;
 import eu.pabl.twitchchat.TwitchChatMod;
 import it.unimi.dsi.fastutil.ints.IntSet;
 import net.minecraft.client.font.Font;
+import net.minecraft.client.font.FontFilterType;
 import net.minecraft.client.font.Glyph;
+import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.stream.IntStream;
+import java.util.List;
 
 public class ChannelFont implements Font {
+    public static final Identifier CHANNEL_ICON_FONT_STORAGE = Identifier.of("twitchchat", "channel_icon2");
+    public static final List<Font.FontFilterPair> CHANNEL_ICON_FONT_FILTER = List.of(new Font.FontFilterPair(new ChannelFont(), FontFilterType.FilterMap.NO_FILTER));
+
     @Override
     public void close() {
         Font.super.close();
--- src/main/java/eu/pabl/twitchchat/mixin/MixinStringRenderOutput.java
+++ src/main/java/eu/pabl/twitchchat/mixin/MixinStringRenderOutput.java
@@ -2,8 +2,6 @@ package eu.pabl.twitchchat.mixin;
 
 import eu.pabl.twitchchat.TwitchChatMod;
 import eu.pabl.twitchchat.channelFont.ChannelFont;
-import net.minecraft.client.font.Font;
-import net.minecraft.client.font.FontFilterType;
 import net.minecraft.client.font.FontManager;
 import net.minecraft.client.font.FontStorage;
 import net.minecraft.client.texture.TextureManager;
@@ -12,7 +10,6 @@ import net.minecraft.resource.ResourceReloader;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -30,18 +27,14 @@ public class MixinStringRenderOutput {
     @Shadow
     private TextureManager textureManager;
 
-    @Unique
-    private static final Identifier channelIconFontID = Identifier.of("twitchchat", "channel_icon2");
-    @Unique
-    private static final List<Font.FontFilterPair> channelIconFontFilter = List.of(new Font.FontFilterPair(new ChannelFont(), FontFilterType.FilterMap.NO_FILTER));
 
     @Inject(method="reload", at=@At("RETURN"))
     public CompletableFuture<Void> afterReload(ResourceReloader.Synchronizer synchronizer, ResourceManager manager, Executor prepareExecutor, Executor applyExecutor, CallbackInfoReturnable<CompletableFuture<Void>> ci) {
         return ci.getReturnValue().thenRun(() -> {
-            FontStorage channelIconFontStorage = new FontStorage(this.textureManager, channelIconFontID);
-            channelIconFontStorage.setFonts(channelIconFontFilter, null);
-            fontStorages.put(channelIconFontID, channelIconFontStorage);
-            TwitchChatMod.LOGGER.info("Added custom channel icon font: " + channelIconFontID);
+            FontStorage channelIconFontStorage = new FontStorage(this.textureManager, ChannelFont.CHANNEL_ICON_FONT_STORAGE);
+            channelIconFontStorage.setFonts(ChannelFont.CHANNEL_ICON_FONT_FILTER, null);
+            fontStorages.put(ChannelFont.CHANNEL_ICON_FONT_STORAGE, channelIconFontStorage);
+            TwitchChatMod.LOGGER.info("Added custom channel icon font: " + ChannelFont.CHANNEL_ICON_FONT_STORAGE);
         });
     }
 }
